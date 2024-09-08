7f5aa4fc8bf95e2a00269865e2b041a21c112441
nzaitseva <web.coder.py@gmail.com>
2024-09-08 23:58:42 +0300

    Create items.py

---
+++ kitchengoods/items.py
@@ -0,0 +1,32 @@
+import scrapy
+
+class KitchengoItem(scrapy.Item):
+    source_url = scrapy.Field()
+    product_category = scrapy.Field()
+    sku = scrapy.Field()
+    jan = scrapy.Field()
+    ean = scrapy.Field()
+    price = scrapy.Field()
+    model = scrapy.Field()
+    quantity = scrapy.Field()
+    image = scrapy.Field()
+    source_image = scrapy.Field()
+    additional_images = scrapy.Field()
+    source_additional_images = scrapy.Field()
+    manufacturer_id = scrapy.Field()
+    manufacturer = scrapy.Field()
+    status = scrapy.Field() #всегда 1 т.к. парсим только в наличии/на складе
+    stock_status = scrapy.Field()  # В наличии, Предзаказ
+    stock_status_id = scrapy.Field()
+    date_added = scrapy.Field()
+    date_modified = scrapy.Field()
+    mpn = scrapy.Field()
+    name_ru = scrapy.Field()
+    description_ru = scrapy.Field()
+    description_params = scrapy.Field()
+    meta_title_ru = scrapy.Field()
+    meta_description_ru = scrapy.Field()
+    meta_keyword_ru = scrapy.Field()
+    meta_h1_ru = scrapy.Field()
+    product_attribute = scrapy.Field()
+
